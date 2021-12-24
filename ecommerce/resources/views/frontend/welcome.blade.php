50f6cb31d996ce718526b383a5c51cd3e4bd88e2
Nafiz <fuadnafiz254@gmail.com>
2021-12-24 12:39:31 +0600

    5th commit

--- ecommerce/resources/views/frontend/welcome.blade.php
+++ ecommerce/resources/views/frontend/welcome.blade.php
@@ -102,8 +102,8 @@
                                             <div class="product-body">
                                                 <p class="product-category">Category</p>
                                                 <h3 class="product-name"><a href="#">{{ $category->name }}</a></h3>
-                                                <h4 class="product-price">$980.00 <del
-                                                        class="product-old-price">$990.00</del></h4>
+                                                <h4 class="product-price">980.00 TK<del
+                                                        class="product-old-price">990.00 TK</del></h4>
                                                 <div class="product-rating">
                                                     <i class="fa fa-star"></i>
                                                     <i class="fa fa-star"></i>
@@ -111,14 +111,14 @@
                                                     <i class="fa fa-star"></i>
                                                     <i class="fa fa-star"></i>
                                                 </div>
-                                                <div class="product-btns">
+                                                {{-- <div class="product-btns">
                                                     <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span
                                                             class="tooltipp">add to wishlist</span></button>
                                                     <button class="add-to-compare"><i class="fa fa-exchange"></i><span
                                                             class="tooltipp">add to compare</span></button>
                                                     <button class="quick-view"><i class="fa fa-eye"></i><span
                                                             class="tooltipp">quick view</span></button>
-                                                </div>
+                                                </div> --}}
                                             </div>
                                             <form action="/add_to_cart" method="POST">
                                                 @csrf
@@ -241,8 +241,8 @@
                                             <div class="product-body">
                                                 <p class="product-category">Category</p>
                                                 <h3 class="product-name"><a href="#">{{ $category->name }}</a></h3>
-                                                <h4 class="product-price">$980.00 <del
-                                                        class="product-old-price">$990.00</del></h4>
+                                                <h4 class="product-price">980.00 TK<del
+                                                        class="product-old-price">990.00 TK</del></h4>
                                                 <div class="product-rating">
                                                     <i class="fa fa-star"></i>
                                                     <i class="fa fa-star"></i>
@@ -250,14 +250,14 @@
                                                     <i class="fa fa-star"></i>
                                                     <i class="fa fa-star"></i>
                                                 </div>
-                                                <div class="product-btns">
+                                                {{-- <div class="product-btns">
                                                     <button class="add-to-wishlist"><i class="fa fa-heart-o"></i><span
                                                             class="tooltipp">add to wishlist</span></button>
                                                     <button class="add-to-compare"><i class="fa fa-exchange"></i><span
                                                             class="tooltipp">add to compare</span></button>
                                                     <button class="quick-view"><i class="fa fa-eye"></i><span
                                                             class="tooltipp">quick view</span></button>
-                                                </div>
+                                                </div> --}}
                                             </div>
                                             <form action="/add_to_cart" method="POST">
                                                 @csrf
